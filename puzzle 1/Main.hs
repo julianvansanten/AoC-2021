74cf58d54bdd999c5b19bf8628392fd170ee6b63
julianvansanten <julianvs0501@gmail.com>
2021-12-01 19:26:14 +0100

    Added comments. Added ability to include file given by program argument.

--- puzzle 1/Main.hs
+++ puzzle 1/Main.hs
@@ -1,34 +1,49 @@
 module Main where
+import System.Environment ( getArgs )
 
-main :: IO ()
-main = do
-    contents <- readFile "./data.txt"
-    let allLines = lines contents
-    let ints = map read allLines
-    let answer1 = getTotalIncreasing ints
-    putStrLn $ "The first answer is: " ++ show answer1
+-- Exercise 1
+-- Count the number of numbers that are larger than the previous in an array.
+countIncreasing :: [Int] -> Int
+countIncreasing list = countTrueValues $ getIncreasingArray list []
 
-    let answer2 = getTotalIncreasing $ getTotalWindow ints
-    putStrLn $ "The second answer is: " ++ show answer2
+-- Create an array of booleans which tell whether the next element is bigger than the previous.
+getIncreasingArray :: [Int] -> [Bool] -> [Bool]
+getIncreasingArray [x,y] bools = bools ++ [y > x]
+getIncreasingArray (x:y:xs) [] | y > x = getIncreasingArray (y:xs) [False, True]
+                        | otherwise = getIncreasingArray (y:xs) [False, False]
+getIncreasingArray (x:y:xs) bools | y > x = getIncreasingArray (y:xs) (bools ++ [True])
+                            | otherwise = getIncreasingArray (y:xs) (bools ++ [False])
+getIncreasingArray _ bools = bools
 
-getTotalIncreasing :: [Int] -> Int
-getTotalIncreasing list = countTrue $ getIncreasing list []
+-- Count the amount of occurrences of True in a given array.
+countTrueValues :: [Bool] -> Int
+countTrueValues = length . filter (True ==)
 
-getIncreasing :: [Int] -> [Bool] -> [Bool]
-getIncreasing [x,y] bools = bools ++ [y > x]
-getIncreasing (x:y:xs) [] | y > x = getIncreasing (y:xs) [False, True]
-                        | otherwise = getIncreasing (y:xs) [False, False]
-getIncreasing (x:y:xs) bools | y > x = getIncreasing (y:xs) (bools ++ [True])
-                            | otherwise = getIncreasing (y:xs) (bools ++ [False])
-getIncreasing _ bools = bools
 
-countTrue :: [Bool] -> Int
-countTrue = length . filter (True ==)
-
+--Exercise 2
+-- Convert a given array to a sliding window of size 3.
 getTotalWindow :: [Int] -> [Int]
 getTotalWindow list = getWindow list []
 
+-- Get the array of windows which contain the sum of some three values.
 getWindow :: [Int] -> [Int] -> [Int]
 getWindow [x,y,z] windows = windows ++ [x+y+z]
 getWindow (x:y:z:xs) windows = windows ++ [x+y+z] ++ getWindow (y:z:xs) windows
 getWindow _ windows = windows
+
+
+-- Read a file with a given name
+main :: IO ()
+main = do
+    fileName <- head <$> getArgs
+    contents <- readFile fileName
+    let allLines = lines contents
+    let ints = map read allLines
+
+    let answer1 = countIncreasing ints
+    putStrLn $ "The first answer is: " ++ show answer1
+
+    putStrLn "\n"
+
+    let answer2 = countIncreasing $ getTotalWindow ints
+    putStrLn $ "The second answer is: " ++ show answer2
