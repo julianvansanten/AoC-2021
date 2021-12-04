361e697d11f54ddf78681f4b90314fac3c3e4082
julianvansanten <julianvs0501@gmail.com>
2021-12-04 21:48:44 +0100

    Added Parser for puzzle 4. Added first logic.

--- puzzle4/Main.hs
+++ puzzle4/Main.hs
@@ -1,4 +1,28 @@
 module Main where
+import System.Environment (getArgs)
+import Parser
+import Text.ParserCombinators.Parsec hiding (spaces)
+import qualified Text.ParserCombinators.Parsec.Token as Token
+
+cardTranspose :: Card -> Card
+cardTranspose (Card rs) = undefined
+
+rowHasBingo :: CardRow -> Bool
+rowHasBingo (CardRow cs) = foldl (\b (CardCell (_,cell)) -> b && cell) True cs
+
+markCard :: Card -> Integer -> Card
+markCard (Card c) i = Card (map (`markRow` i) c)
+
+markRow :: CardRow -> Integer -> CardRow
+markRow (CardRow r) i = CardRow (map (`markCell` i) r)
+
+markCell :: CardCell -> Integer -> CardCell
+markCell (CardCell(a, b)) i | a == i = CardCell (a, True)
+                    | otherwise = CardCell (a, False)
 
 main = do
-    putStrLn "Hello, World!"
+    fileName <- head <$> getArgs
+    contents <- readFile fileName
+    case parse parseFile "" contents of
+            Left err  -> print err
+            Right xs  -> print xs
---
+++ puzzle4/Parser.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE TupleSections #-}
+module Parser where
+import Text.ParserCombinators.Parsec hiding (spaces)
+import qualified Text.ParserCombinators.Parsec.Token as Token
+
+-- EDSL for a bingo game
+newtype CardCell = CardCell (Integer, Bool)
+    deriving (Show, Eq)
+newtype CardRow = CardRow [CardCell]
+    deriving (Show, Eq)
+newtype Card = Card [CardRow]
+    deriving (Show, Eq)
+newtype Draws = Draws [Integer]
+    deriving (Show, Eq)
+data BingoGame = BingoGame Draws [Card]
+    deriving (Show, Eq)
+
+-- A whitespace parser
+spaces :: Parser ()
+spaces = skipMany1 space
+
+-- A parser for the list of ball draws
+parseDraws :: Parser Draws
+parseDraws = Draws . map read <$> x
+    where x = many1 digit `sepBy1` (char ',' <* skipMany space)
+
+parseCell :: Parser CardCell
+parseCell = CardCell . (,False) . read <$> x
+    where x = many (char ' ') *> many1 digit
+
+-- A parser for a single row of a bingo card
+parseRow :: Parser CardRow
+parseRow = CardRow <$> x
+    where x = parseCell `sepBy1` many1 (char ' ')
+
+-- A parser for an entire bingo card
+parseCard :: Parser Card
+parseCard = Card <$> parseRow `sepEndBy1` newline
+
+-- Parse a file to a BingoGame
+parseFile :: Parser BingoGame
+parseFile = BingoGame <$> parseDraws <* spaces <*> (parseCard `sepBy1` many newline)
+
