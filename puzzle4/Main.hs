7aee6793997d8560f75a6978c920509f8556848a
julianvansanten <julianvs0501@gmail.com>
2021-12-05 15:16:08 +0100

    Added transpose for a bingo card.

--- puzzle4/Main.hs
+++ puzzle4/Main.hs
@@ -3,9 +3,23 @@ import System.Environment (getArgs)
 import Parser
 import Text.ParserCombinators.Parsec hiding (spaces)
 import qualified Text.ParserCombinators.Parsec.Token as Token
+import Control.Monad.Trans.Except (except)
 
 cardTranspose :: Card -> Card
-cardTranspose (Card rs) = undefined
+cardTranspose (Card ((CardRow []):rs)) = Card []
+cardTranspose (Card x) = h `combineRows` t
+    where   h = CardRow (map rowHead x)
+            t = cardTranspose $ Card (map rowTail x)
+
+combineRows :: CardRow -> Card -> Card
+combineRows r (Card rs) = Card (r:rs)
+
+rowHead :: CardRow -> CardCell
+rowHead (CardRow []) = error "Empty CardRow"
+rowHead (CardRow (x:xs)) = x
+rowTail :: CardRow -> CardRow
+rowTail (CardRow []) = error "Empty CardRow"
+rowTail (CardRow (x:xs)) = CardRow xs
 
 rowHasBingo :: CardRow -> Bool
 rowHasBingo (CardRow cs) = foldl (\b (CardCell (_,cell)) -> b && cell) True cs
